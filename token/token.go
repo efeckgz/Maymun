95cb7650e95a3baa907882e5b26b672c243467cd
Efe <efeckgz@gmail.com>
2024-01-25 02:25:22 +0300

    test commit ignore

---
+++ token/token.go
@@ -0,0 +1,35 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF 	= "EOF"
+
+	// Identifiers + literals
+	IDENT 	= "IDENT"
+	INT	= "INT"
+
+	// Operators
+	ASSIGN 	= "="
+	PLUS	= "+"
+
+	// Delimiters
+	COMMA	  = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET	 = "LET"
+)
+
+type Token string
+
+type TokenType struct {
+	Type 	TokenType
+	Literal string
+}
+
